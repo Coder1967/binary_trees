da624fe6da07de09e79bd0bc1fc0170531cea705
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-09-22 19:46:07 +0000

    fixing errors

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,7 +8,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL || (tree->right == NULL && tree->left == NULL))
 		return (0);
-	if (tree->left || tree->right)
-		return (1);
-	binary_tree_nodes(tree->right + tree->left);
+	return binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1;
 }
